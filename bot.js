a01521f264ee99f7d5ffaa0bf36e1400a0a6acdb
Sckz <57728145+Sckz@users.noreply.github.com>
2022-05-31 23:17:19 +0200

    Event LFL Update

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const {Client, Intents, Collection, MessageEmbed} = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]})
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]})
 const prefix = "!"
 let axios = require('axios');
 let riotAPI = "RGAPI-ef565d89-e835-41d8-bfd4-7379db0ff1bd";
@@ -39,7 +39,6 @@ client.on('messageCreate', async (msg) => {
         msg.delete()
         msg.channel.send(':space_invader: **__Skins 12.10: __**\n\n**__Skins__** : http://leagueskins.go.yj.fr/\n\n`By KTS CORP`')
     }
-    
 })
 client.on('messageCreate', msg => {
     const args = msg.content.slice(prefix.length).split(/ +/);
@@ -71,6 +70,99 @@ client.on('messageCreate', msg => {
          .setColor("WHITE")
          .setFooter({text: client.user.username})
         msg.channel.send({embeds: [embed]})
+    }
+})
+client.on('messageCreate', async msg => {
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g)
+    if (args[0] == "event") {
+        if (args[1] == "rules") {
+            let rules = new MessageEmbed()
+             .setAuthor({
+                 name: "Event",
+                 iconURL: "https://yt3.ggpht.com/R-LnoDWNxzdEqdiNvH1yoOAKNaRwlgZSFoC8-HBjae97HLESiu2cbE27uJtyeHAg5u44ySi-1w=s900-c-k-c0x00ffffff-no-rj"
+            })
+             .setTitle("LFL - Event")
+             .setDescription("Bienvenue sur le salon de l'événement **LFL** de ce serveur qui débutera dès le premier match sois le Mercredi 01 Juin.\n\n**__Comment ça marche ? :__**\nDans ce salon vont être postés les matchs. Vous aurez jusqu'à la fin de la Draft pour voter en réagissant sur les émojis en dessous. Celui qui a le plus de points gagne un role. Pour voir vos points il suffit de faire la commande `!event` et va vous renvoyer un lien vers la liste des points.\n\nBonne chance !")
+             .setColor("RED")
+             .setTimestamp()
+             .setFooter({text: client.user.username})
+            msg.channel.send({embeds: [rules]})
+        }
+        if (args[1] == "day1") {
+            msg.delete()
+            let m1 = new MessageEmbed()
+             .setAuthor({
+                 name: "Event",
+                 iconURL: "https://yt3.ggpht.com/R-LnoDWNxzdEqdiNvH1yoOAKNaRwlgZSFoC8-HBjae97HLESiu2cbE27uJtyeHAg5u44ySi-1w=s900-c-k-c0x00ffffff-no-rj"
+            })
+             .setTitle("LFL - Jour 1 Semaine 1")
+             .setDescription('**__18h - BDSA V/S LDLC OL__**\n\n*Votez en réagissant ci-dessous !*')
+             .setColor("GOLD")
+             .setTimestamp()
+             .setFooter({text: client.user.username})
+            let s1 = await msg.channel.send({embeds: [m1]})
+             s1.react('<:BDSA:981239618882117732>')
+             s1.react('<:LDLCOL:981239591027757056>')
+             
+            let m2 = new MessageEmbed()
+             .setAuthor({
+                 name: "Event",
+                 iconURL: "https://yt3.ggpht.com/R-LnoDWNxzdEqdiNvH1yoOAKNaRwlgZSFoC8-HBjae97HLESiu2cbE27uJtyeHAg5u44ySi-1w=s900-c-k-c0x00ffffff-no-rj"
+            })
+             .setTitle("LFL - Jour 1 Semaine 1")
+             .setDescription('**__19h - GO VS Solary__**\n\n*Votez en réagissant ci-dessous !*')
+             .setColor("WHITE")
+             .setTimestamp()
+             .setFooter({text: client.user.username})
+            let s2 = await msg.channel.send({embeds: [m2]})
+            s2.react('<:GO:981239610002796624>')
+            s2.react('<:SLY:981239640310829056>')
+
+            let m3 = new MessageEmbed()
+             .setAuthor({
+                 name: "Event",
+                 iconURL: "https://yt3.ggpht.com/R-LnoDWNxzdEqdiNvH1yoOAKNaRwlgZSFoC8-HBjae97HLESiu2cbE27uJtyeHAg5u44ySi-1w=s900-c-k-c0x00ffffff-no-rj"
+            })
+             .setTitle("LFL - Jour 1 Semaine 1")
+             .setDescription('**__20h - Misfits Premier V/S GameWard__**\n\n*Votez en réagissant ci-dessous !*')
+             .setColor("GOLD")
+             .setTimestamp()
+             .setFooter({text: client.user.username})
+            let s3 = await msg.channel.send({embeds: [m3]})
+            s3.react('<:MSF:981239591153573959>')
+            s3.react('<:GW:981239591103266866>')
+
+            let m4 = new MessageEmbed()
+            .setAuthor({
+                name: "Event",
+                iconURL: "https://yt3.ggpht.com/R-LnoDWNxzdEqdiNvH1yoOAKNaRwlgZSFoC8-HBjae97HLESiu2cbE27uJtyeHAg5u44ySi-1w=s900-c-k-c0x00ffffff-no-rj"
+           })
+            .setTitle("LFL - Jour 1 Semaine 1")
+            .setDescription('**__21h - Team Oplon VS Karmine Corp__**\n\n*Votez en réagissant ci-dessous !*')
+            .setColor("WHITE")
+            .setTimestamp()
+            .setFooter({text: client.user.username})
+           let s4 = await msg.channel.send({embeds: [m4]})
+           s4.react('<:OPL:981239628361244733>')
+           s4.react('<:KC:981239591166177320>')
+
+           let m5 = new MessageEmbed()
+            .setAuthor({
+                name: "Event",
+                iconURL: "https://yt3.ggpht.com/R-LnoDWNxzdEqdiNvH1yoOAKNaRwlgZSFoC8-HBjae97HLESiu2cbE27uJtyeHAg5u44ySi-1w=s900-c-k-c0x00ffffff-no-rj"
+           })
+            .setTitle("LFL - Jour 1 Semaine 1")
+            .setDescription('**__22h - Vitality.Bee V/S Mirage-Elyandra__**\n\n*Votez en réagissant ci-dessous !*')
+            .setColor("GOLD")
+            .setTimestamp()
+            .setFooter({text: client.user.username})
+           let s5 = await msg.channel.send({embeds: [m5]})
+           s5.react('<:VIT:981239650620416000>')
+           s5.react('<:ELY:981239591031951450>')
+        }
+        else {
+            msg.channel.send("http://leagueskins.go.yj.fr/event/points.html")
+        }
     }
 })
 
